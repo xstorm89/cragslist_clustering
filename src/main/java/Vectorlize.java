474fd6b196dc01a419c782a537b7d1f70a4e2f55
xstorm89 <xuxuncharles@gmail.com>
2015-05-27 13:46:41 -0700

    3

--- src/main/java/Vectorlize.java
+++ src/main/java/Vectorlize.java
@@ -18,7 +18,7 @@ public class Vectorlize {
 
         String text ="$5000 On this day in 1945, U.S. Marines captured Iwo Jima's Mount Suribachi in one of the bloodiest, most famous battles of World War II. The historic Associated Press photo of Marines raising the flag was also taken on this day.";
         StandoffNamedEntityRecognizer entityRecognizer = new StandoffNamedEntityRecognizer("classifiers/english.muc.7class.distsim.crf.ser.gz");
-       entity= entityRecognizer.getEntity(text,"qwsad");
+       entity= entityRecognizer.getEntity(text,"MONEY");
 
         System.out.println(entity.toString());
     }
